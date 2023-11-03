9ed1538c9bd79738551b2bb3826dec744196aba9
Bavalics Marcell <marcellbavalics11@gmail.com>
2023-11-03 15:21:24 +0100

    5feladat 1

---
+++ 5feladat.py
@@ -0,0 +1,30 @@
+'''
+for paros_szamok in range(2,12,2):
+    print(paros_szamok,end=' ')
+'''
+
+'''
+for i in range(10,0,-1):
+    print(i,end=' ')
+'''
+
+'''
+for a in range(9,0,-2):
+    print(a,end=' ')
+'''
+
+'''
+a = input("Írj be egy szöveget: ")
+b = int(input("Írd le, hogy hányszor írjam le: "))
+for b in range(b):
+    print(a)
+'''
+'''
+while True:
+    szam = int(input("Kérlek adj meg egy páros számot: "))
+    if szam % 2 == 0:
+        print("A megadott szám páros.")
+        break
+    else:
+        print("Adj egy számot megint.")
+'''
